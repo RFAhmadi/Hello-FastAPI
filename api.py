550aaf8f2d7de70f67b9e387fedf7e8e281028db
arefogit <aref.ahmadi.ce@gmail.com>
2024-08-30 00:59:20 +0330

    added api.py

---
+++ api.py
@@ -0,0 +1,11 @@
+import requests
+from config_weather import API_KEY
+
+
+def api_get_weather(city):
+    """
+    this function returns the weather information for the given city
+    """
+    url = f"https://api.weatherapi.com/v1/current.json?key={API_KEY}&q={city}&aqi=no"
+    response = requests.get(url)
+    return response.json()
